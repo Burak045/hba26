eaf1e88698f97f7547430e50268417be01420ffd
Burak045 <asik.burak07@gmail.com>
2026-02-24 22:21:59 +0300

    if.kullanımı.örneği
    saatlere ve cinsiyete göre selamlama metni

--- ilk.js
+++ ilk.js
@@ -1,1 +1,27 @@
 // Hüseyin Burak Aşık 
+const cinsiyetler = ['erkek', 'kadın'];
+const cinsiyet = cinsiyetler[Math.floor(Math.random() * cinsiyetler.length)];
+
+const saat =  Math.floor(Math.random() * 24);
+
+
+let mesaj = "";
+
+if( saat < 6 ){
+  mesaj = "İyi geceler";
+}else if (saat >=6 && saat < 12) {
+  mesaj = "Günaydin";
+}else if ( saat >= 12 && saat < 18) {
+  mesaj =  "İyi günler";
+}else{
+  mesaj = "iyi akşamlar";
+}
+ 
+ if( cinsiyet == "erkek") {
+  mesaj =  mesaj + " bayım";
+} else {
+   mesaj = mesaj +  " hanımefendi";
+}
+
+console.log("cinsiyet:" , cinsiyet, "Saat:", saat);
+console.log(mesaj);
