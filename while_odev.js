6906a7e6deca8ccc8e9298fc21b7a7d527104e1c
Burak045 <asik.burak07@gmail.com>
2026-02-26 14:33:25 +0300

    ödevi yaptım

--- while_odev.js
+++ while_odev.js
@@ -2,3 +2,13 @@
 while döngüsü okunacak 
 arabalar örneği while ile yazdırılacak
 */
+
+
+const cars = ["BMW ", "Audi ", "Mercedes ", "Ford ", "Aston Martin ", "Toyota", "Ferrari"];
+let x = 0;
+while (x < cars.length) {
+  console.log("Marka:", cars[x]);
+  x++
+}
+
+
